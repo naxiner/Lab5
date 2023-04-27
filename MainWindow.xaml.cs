d08f8b1eaec190da6dcf639a6ee72c41abe09e07
naxiner <naxiner@WIN-RFK91QGHMFQ>
2023-04-27 18:22:22 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab5
+{
+    public partial class MainWindow : Window
+    {
+        string logPath = "logs\\log.json";          // шлях до файлу логів
+
+        University university = new University();   // створення екземпляру класу університету
+        int subjectCount = 7;                       // початкова кількість предметів
+        int teachersCount = 10;                     // початкова кількість вчителів
+
+        List<Thread> threads = new List<Thread>();  // список потоків
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            university.AddSubjects(subjectCount);
+            university.AddTeachers(teachersCount);
+        }
+
+        // метод Refresh - оновлює всі 3 текстові поля
+        public void Refresh()
+        {
+            tb_Students.Clear();
+            tb_Teachers.Clear();
+            tb_Subjects.Clear();
+            try
+            {
+                foreach (var student in university.Students)
+                {
+                    tb_Students.Text += student.Name + '\n';
+                }
+                foreach (var teacher in university.Teachers)
+                {
+                    tb_Teachers.Text += teacher.Name + '\n';
+                }
+                foreach (var subject in university.Subjects)
+                {
+                    tb_Subjects.Text += subject.Name + '\n';
+                }
+            }
+            catch 
+            {
+                Refresh();
+            }
+        }
+
+        // обробник кнопки Start
+        private async void bn_Start_Click(object sender, RoutedEventArgs e)
+        {
+            int durationTime;   // затримка для потоків (секунди)
+            try
+            {
+                // спроба прочитати з текстового поля число
+                durationTime = Convert.ToInt32(tb_DurationTime.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Потрібно ввести ціле число в поле затримки!", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            
+            // поток початку вступної компанії
+            Thread threadStartAC = new Thread(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(durationTime));
+                    university.StartAdmissionCamping();
+                }
+                
+            });
+            threads.Add(threadStartAC);
+
+            // поток початку канікул
+            Thread threadStartVC = new Thread(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(durationTime));
+                    university.StartVacationPeriod();
+                }
+            });
+            threads.Add(threadStartVC);
+
+            // поток початку навчання
+            Thread threadStartC = new Thread(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(durationTime));
+                    university.StartClasses();             
+                }
+            });
+            threads.Add(threadStartC);
+
+            threadStartAC.Start();
+            threadStartVC.Start();
+            threadStartC.Start();
+            
+            // оновлення всіх трьох текстових полів, а також запис у логи кожні n секунд
+            while (true)
+            {
+                await Task.Delay(durationTime * 1000);
+                university.SaveStateLog(logPath);
+                Refresh();
+            }
+        }
+
+        // обробник кнопки Stop
+        private void bn_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            // зупинити усі потоки
+            foreach (var thread in threads)
+            {
+                thread.Abort();
+            }
+        }
+
+        // обробник кнопки Refresh
+        private void bn_Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+    }
+}
---
+++ University.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+
+
+namespace Lab5
+{
+    internal class University
+    {
+        public List<Teacher> Teachers { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public University()
+        {
+            Teachers = new List<Teacher>();
+            Students = new List<Student>();
+            Subjects = new List<Subject>();
+        }
+
+        // метод періоду вступної компанії
+        public void StartAdmissionCamping()
+        {
+            int countOfStudents = 25;
+            AddStudents(countOfStudents);   // зарахування студентів
+            ExpelStudents();                // відрахування студентів
+        }
+
+        // метод періоду відпусток
+        public void StartVacationPeriod() 
+        {
+            int countOfSubjects = 2;
+            RemoveSubjects();
+            AddSubjects(countOfSubjects);
+            ExpelTeachers();         
+        }
+
+        // метод періоду навчання
+        public void StartClasses()
+        {
+            int countOfTeachers = 2;
+            AddTeachers(countOfTeachers);
+        }
+
+        // метод для додавання випадкових даних про студентів
+        public void AddStudents(int count)
+        {
+            // список існуючих імен для рандомної вибірки
+            var names = new List<string>()
+            {
+                "Adam", "Alex", "Aaron", "Benjamin", "Connor", "Cameron", "David", "Daniel",
+                "Ethan", "Edward", "Fred", "Frank", "George", "Gerald", "Hank", "Henry",
+                "Ivan", "Isaac", "Jerry", "Jack", "Kevin", "Kenneth", "Larry", "Leonard",
+                "Michael", "Matthew", "Mark", "Nathan", "Nicholas", "Oliver", "Oscar", "Peter",
+                "Patrick", "Paul", "Quentin", "Robert", "Richard", "Roger", "Samuel", "Seth",
+                "Thomas", "Timothy", "Ulysses", "Victor", "Vincent", "William", "Wesley", "Xavier",
+                "Zachary", "Zeke"
+            };
+
+            var rnd = new Random();
+
+            // створення потрібної кількості студентів
+            for (int i = 0; i < count; i++)
+            {
+                var name = names[rnd.Next(names.Count)];
+                var student = new Student(name);
+
+                var numSubjects = rnd.Next(1, Subjects.Count);
+                var subjects = Subjects.OrderBy(x => rnd.Next()).Take(numSubjects).ToList();
+                student.Subjects.AddRange(subjects);
+
+                Students.Add(student);
+            }
+        }
+
+        // метод для відрахування студентів
+        public void ExpelStudents()
+        {
+            if (Students.Count > 1)
+            {
+                Random random = new Random();
+                int numberToExpel = random.Next(1, 20);
+
+                for (int i = 0; i < numberToExpel; i++)
+                {
+                    Students.RemoveAt(random.Next(Students.Count));
+                }
+            }
+        }
+
+
+        // метод для додавання випадкових даних про викладачів
+        public void AddTeachers(int count)
+        {
+            // список існуючих імен для рандомної вибірки
+            var names = new List<string>()
+            {
+                "Adam", "Alex", "Aaron", "Benjamin", "Connor", "Cameron", "David", "Daniel",
+                "Ethan", "Edward", "Fred", "Frank", "George", "Gerald", "Hank", "Henry",
+                "Ivan", "Isaac", "Jerry", "Jack", "Kevin", "Kenneth", "Larry", "Leonard",
+                "Michael", "Matthew", "Mark", "Nathan", "Nicholas", "Oliver", "Oscar", "Peter",
+                "Patrick", "Paul", "Quentin", "Robert", "Richard", "Roger", "Samuel", "Seth",
+                "Thomas", "Timothy", "Ulysses", "Victor", "Vincent", "William", "Wesley", "Xavier",
+                "Zachary", "Zeke"
+            };
+
+            var rnd = new Random();
+
+            // створення потрібної кількості викладачів
+            for (int i = 0; i < count; i++)
+            {
+                var name = names[rnd.Next(names.Count)];
+                var teacher = new Teacher(name);
+
+                Teachers.Add(teacher);
+            }
+        }
+
+        // метод для звільнення викладачів
+        public void ExpelTeachers()
+        {
+            if (Teachers.Count > 1)
+            {
+                Random random = new Random();
+                int numberToExpel = random.Next(1, 2);
+
+                for (int i = 0; i < numberToExpel; i++)
+                {
+                    Teachers.RemoveAt(random.Next(Teachers.Count));
+                }
+            }
+        }
+
+        // метод для додавання випадкових даних про предмети
+        public void AddSubjects(int count)
+        {
+            // список існуючих назв для рандомної вибірки
+            var names = new List<string>()
+            {
+                "Mathematics", "Physics", "Chemistry", "Biology", "History", "Geography", "English",
+                "French", "German", "Spanish", "Computer Science", "Art", "Music", "Physical Education"
+            };
+
+            var rnd = new Random();
+
+            // створення потрібної кількості предметів
+            for (int i = 0; i < count; i++)
+            {
+                var name = names[rnd.Next(names.Count)];
+                var subject = new Subject(name);
+
+                Subjects.Add(subject);
+            }
+        }
+
+        // метод видалення предметів
+        public void RemoveSubjects()
+        {
+            if (Subjects.Count > 3)
+            {
+                Random random = new Random();
+                int numberToExpel = random.Next(1, 2);
+
+                for (int i = 0; i < numberToExpel; i++)
+                {
+                    Subjects.RemoveAt(random.Next(Subjects.Count));
+                }
+            }
+        }
+
+
+        // метод для збереження логу стану університету у файл
+        public void SaveStateLog(string filePath)
+        {
+            // налаштування логування в файл в форматі JSON
+            Log.Logger = new LoggerConfiguration().WriteTo.File(new JsonFormatter(), 
+                filePath, rollingInterval: RollingInterval.Day).CreateLogger();
+
+            // запис наявних даних у файл
+            foreach (var student in Students)
+            {
+                Log.Information("Student: " + student.Name);
+            }
+            foreach (var teacher in Teachers)
+            {
+                Log.Information("Teacher: " + teacher.Name);
+            }
+            foreach (var subject in Subjects)
+            {
+                Log.Information("Subject: " + subject.Name);
+            }
+
+            // закриття логера 
+            Log.CloseAndFlush();
+        }
+    }
+
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public Teacher(string name)
+        {
+            Name = name;
+            Subjects = new List<Subject>();
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+            Subjects = new List<Subject>();
+        }
+    }
+
+    public class Subject
+    {
+        public string Name { get; set; }
+
+        public Subject(string name)
+        {
+            Name = name;
+        }
+    }
+}
